2787b7e1d911bdd520606ab55eb0bdc67d0b0d42
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2023-03-21 13:27:55 +0100

    feat(): update docs; cleanup code; attempt to add repo name as input from process

--- README.md
+++ README.md
@@ -1,7 +1,44 @@
 # LensDX Deployment Action
 
-TODO
+An action to informs LensDX that you have deployed your service - helps keep your statistics up to date and in order!
 
-## References
+## Setup and usage
 
-- https://docs.github.com/en/actions/using-workflows/events-that-trigger-workflows
+You need to set a required secret for an API key, then you are greenlit to just start using the action!
+
+### Remember...
+
+- Always ensure you have secure settings regarding what actions you allow.
+- Note that Dorametrix will _not_ work without access to the Git history (i.e. `with.fetch-depth: 0`).
+
+## Required input arguments
+
+### `api-key`
+
+Dorametrix API token.
+
+## Environment variables the action uses
+
+This action will use `${{ github.event.repository.name }}` to grab the repository name.
+
+## An example of how to use this action in a workflow
+
+```yml
+on: [push]
+
+jobs:
+  dorametrix:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v3
+        with:
+          fetch-depth: 0 # We need to have access to the full Git history
+
+      # Do your things here: build, test, deploy...
+
+      - name: Run LensDX Deployment action
+        uses: lensdx/lensdx-deployment-action@v0
+        with:
+          api-key: ${{ secrets.LENSDX_DEPLOYMENT_API_KEY }} # Use whatever secret name you want!
+```
--- deployment.sh
+++ deployment.sh
@@ -2,57 +2,41 @@
 
 set -o pipefail
 
-#echo "✨ Running Dorametrix deployment event script..."
-
-# Set variables
-ENDPOINT="https://dorametrix.lensdx.app"
-
 API_KEY="${1}"
-if [ -z "$API_KEY" ]; then echo "Error: API key is not set! Exiting..." && exit 1; fi
+if [ -z "$API_KEY" ]; then echo "LensDX error: API key is not set! Exiting..." && exit 1; fi
 
-#if [ -z "$REPO_NAME" ]; then REPO_NAME="$3"; fi # Input from user when calling the action
-REPO_NAME="SOMEORG/SOMEREPO"
-#echo "ℹ️ REPO_NAME --> $REPO_NAME"
-#if [ -z "$REPO_NAME" ]; then echo "Dorametrix error: REPO_NAME is not set! Exiting..." && exit 1; fi
+REPO_NAME="${2}"
+echo "ℹ️ REPO_NAME --> $REPO_NAME"
+if [ -z "$REPO_NAME" ]; then echo "LensDX error: REPO_NAME is not set! Exiting..." && exit 1; fi
+
+ENDPOINT="https://dorametrix.lensdx.app"
 
 # Get current Git SHA
 CURRENT_GIT_SHA=$(git log --pretty=format:'%H' -n 1)
-#echo "ℹ️ CURRENT_GIT_SHA --> $CURRENT_GIT_SHA"
 
 # Get commit ID of last production deployment
 LAST_PROD_DEPLOY=$(curl "$ENDPOINT/lastdeployment?product=$REPO_NAME" -H 'Authorization: "$API_KEY"' | jq '.id' -r)
 
-# If no LAST_PROD_DEPLOY is found, then very defensively assume that the first commit is most recent deployment
 if [[ -z "$LAST_PROD_DEPLOY" ]] || [[ "$LAST_PROD_DEPLOY" == "null" ]]; then
-  #echo "⚠️ Dorametrix warning: Could not find a value for LAST_PROD_DEPLOY. Setting LAST_PROD_DEPLOY to the value of the first commit."
+  # If no LAST_PROD_DEPLOY is found, then very defensively assume that the first commit is most recent deployment
   LAST_PROD_DEPLOY=$(git rev-list HEAD | tail -n 1)
 fi
-#echo "ℹ️ LAST_PROD_DEPLOY --> $LAST_PROD_DEPLOY"
 
-#echo "Verifying that commits exist..."
-if ! git --no-pager log $LAST_PROD_DEPLOY..$CURRENT_GIT_SHA --decorate=short --pretty=oneline; then
-  #echo "🔥 Dorametrix error: Unable to find the expected commits in working tree! Exiting..."
-  exit 1
-fi
+# Verify that commits exist
+if ! git --no-pager log $LAST_PROD_DEPLOY..$CURRENT_GIT_SHA --decorate=short --pretty=oneline; then exit 1; fi
 
 # Get all commits between current work and last production deployment then put result in local TXT file
 git log $LAST_PROD_DEPLOY..$CURRENT_GIT_SHA --pretty=format:'{%n  ^^^^id^^^^: ^^^^%H^^^^,%n  ^^^^timeCreated^^^^: ^^^^%ct^^^^%n  },' | sed 's/"/\\"/g' | sed 's/\^^^^/"/g' | sed "$ s/,$//" | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g' | awk 'BEGIN { print("[") } { print($0) } END { print("]") }' >commits.json
 
 # Use TXT output to set variable with list of commits
 CHANGES=$(cat commits.json | jq '[.[] | { id: .id, timeCreated: .timeCreated }]')
-#echo "ℹ️ CHANGES --> $CHANGES"
 CHANGES_LENGTH=$(echo $CHANGES | jq '. | length' -r)
-#echo "ℹ️ CHANGES_LENGTH --> $CHANGES_LENGTH"
 
 # Remove the scratch TXT file
 rm commits.json
 
-if [[ $CHANGES_LENGTH -eq 0 ]]; then
-  #echo "🔥 Dorametrix error: No changes detected. Exiting..."
-  exit 1
-fi
+# No changes found
+if [[ $CHANGES_LENGTH -eq 0 ]]; then exit 1; fi
 
-# Call Dorametrix and create deployment event with Git changes
-curl -X POST $ENDPOINT/event?authorization="$API_KEY" -d '{ "eventType": "deployment", "repo": "'$REPO_NAME'", "changes": '"$CHANGES"' }' -H "Content-Type: application/json"
-
-echo -e "\n✅ LensDX deployment script has finished successfully!"
+# Call LensDX and create deployment event with Git changes
+curl -X POST $ENDPOINT/event?authorization="$API_KEY" -d '{ "eventType": "deployment", "repo": "'$REPO_NAME'", "changes": '"$CHANGES"' }' -H "Content-Type: application/json" -s
